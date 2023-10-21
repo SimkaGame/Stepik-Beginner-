14f098d40f5e573db63a8b6a7528f6d226ed5ec4
SimkaGame <131748949+SimkaGame@users.noreply.github.com>
2023-10-21 09:20:23 +0300

    Create check_treugolnik_na storony.py

---
+++ check_treugolnik_na storony.py
@@ -0,0 +1,12 @@
+a, b, c = int(input()), int(input()), int(input())
+
+if a == b == c:
+    print("Равносторонний")
+elif a == b != c:
+    print("Равнобедренный")
+elif a != b == c:
+    print("Равнобедренный")
+elif a == c != b:
+    print("Равнобедренный")
+else:
+    print("Разносторонний")
